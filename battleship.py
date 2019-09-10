286ebef53c7c7cb9e0a56126aaad73ae97b6bd50
Rebecca Wallander <sakcheen@gmail.com>
2019-09-10 15:51:13 +0200

    Classify

--- battleship.py
+++ battleship.py
@@ -4,7 +4,38 @@ colors = {"red": (255, 0, 0), "green": (0, 255, 0), "blue": (0, 0, 255), "black"
 screen_height = 1024
 screen_width = 768
 fps = 30
-line_width = int(screen_height / 800)
+
+
+class Pos:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Ship:
+    def __init__(self, color, name: str, size: int, position: Pos):
+        self.color = color
+        self.size = size
+        self.vertical = False
+        self.type = name
+        self.position = position
+
+
+class Grid:
+    def __init__(self, bg_color, line_color, columns: int, rows: int, block_size, screen_pos: Pos, line_width):
+        self.bg_color = bg_color
+        self.line_color = line_color
+        self.columns = columns
+        self.rows = rows
+        self.block_size = block_size
+        self.screen_pos = screen_pos
+        self.line_width = line_width
+
+    def line_area(self) -> pygame.rect.Rect:
+        return pygame.rect.Rect(self.screen_pos.x - self.line_width,
+                                self.screen_pos.y - self.line_width,
+                                (self.block_size + self.line_width) * self.columns + self.line_width,
+                                (self.block_size + self.line_width) * self.rows + self.line_width)
 
 
 def main():
@@ -13,29 +44,20 @@ def main():
 
     screen = pygame.display.set_mode((screen_height, screen_width))
 
-    grid_ship = {"bg_color": colors.get("green"),
-                 "line_color": colors.get("black"),
-                 "columns": 10,
-                 "rows": 10,
-                 "block_size": round(screen_width / 30),
-                 "screen_pos": (0, 0),
-                 "line_width": 1
-                 }
+    grid_ship = Grid(colors.get("green"), colors.get("black"), 10, 10, round(screen_width / 30), Pos(0, 0), 1)
+    grid_enemy = Grid(colors.get("white"), colors.get("black"), 10, 10, round(screen_width / 20), Pos(300, 300), 1)
 
-    ships = {"carrier": ship_carrier(grid_ship["block_size"], (600, 100), grid_ship["line_width"]),
-             "battleship": ship_battleship(grid_ship["block_size"], (600, 125), grid_ship["line_width"]),
-             "destroyer": ship_destroyer(grid_ship["block_size"], (600, 150), grid_ship["line_width"]),
-             "submarine": ship_submarine(grid_ship["block_size"], (600, 175), grid_ship["line_width"]),
-             "patrol_boat": ship_patrol_boat(grid_ship["block_size"], (600, 200), grid_ship["line_width"])}
+    ships = [Ship(colors.get("blue"), "carrier", 5, Pos(600, 100)),
+             Ship(colors.get("blue"), "battleship", 4, Pos(600, 150)),
+             Ship(colors.get("blue"), "destroyer", 3, Pos(600, 200)),
+             Ship(colors.get("blue"), "submarine", 3, Pos(600, 250)),
+             Ship(colors.get("blue"), "patrol_boat", 2, Pos(600, 300))]
 
-    grid_enemy = {"bg_color": colors.get("white"),
-                  "line_color": colors.get("black"),
-                  "columns": 10,
-                  "rows": 10,
-                  "block_size": round(screen_width / 20),
-                  "screen_pos": (300, 300),
-                  "line_width": 1
-                  }
+    ships = {"carrier": ship_carrier(grid_ship.block_size, (600, 100), grid_ship.line_width),
+             "battleship": ship_battleship(grid_ship.block_size, (600, 125), grid_ship.line_width),
+             "destroyer": ship_destroyer(grid_ship.block_size, (600, 150), grid_ship.line_width),
+             "submarine": ship_submarine(grid_ship.block_size, (600, 175), grid_ship.line_width),
+             "patrol_boat": ship_patrol_boat(grid_ship.block_size, (600, 200), grid_ship.line_width)}
 
     ship_dragging = False
 
@@ -66,13 +88,13 @@ def main():
             elif event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     if ship_dragging:
-                        # If inside grid
-                        ships[ship_dragging].x = (round(
-                            ships[ship_dragging].x / (grid_ship["block_size"] + grid_ship["line_width"]))) * (
-                                                             grid_ship["block_size"] + grid_ship["line_width"])
-                        ships[ship_dragging].y = (round(
-                            ships[ship_dragging].y / (grid_ship["block_size"] + grid_ship["line_width"]))) * (
-                                                             grid_ship["block_size"] + grid_ship["line_width"])
+                        if grid_ship.line_area().colliderect(ships[ship_dragging]):
+                            ships[ship_dragging].x = (round(
+                                ships[ship_dragging].x / (grid_ship.block_size + grid_ship.line_width))) * (
+                                                             grid_ship.block_size + grid_ship.line_width)
+                            ships[ship_dragging].y = (round(
+                                ships[ship_dragging].y / (grid_ship.block_size + grid_ship.line_width))) * (
+                                                             grid_ship.block_size + grid_ship.line_width)
                         ship_dragging = False
 
             elif event.type == pygame.MOUSEMOTION:
@@ -114,19 +136,15 @@ def ship_patrol_boat(size, pos, spacing):
     return pygame.rect.Rect(pos[0], pos[1], (size + spacing) * 2 - spacing, size)
 
 
-def draw_grid(win, grid):
-    bg = pygame.rect.Rect(grid["screen_pos"][0] - grid["line_width"],
-                          grid["screen_pos"][1] - grid["line_width"],
-                          (grid["block_size"] + grid["line_width"]) * grid["columns"] + grid["line_width"],
-                          (grid["block_size"] + grid["line_width"]) * grid["rows"] + grid["line_width"])
-    pygame.draw.rect(win, grid["line_color"], bg)
+def draw_grid(win, grid: Grid):
+    pygame.draw.rect(win, grid.line_color, grid.line_area())
 
-    for row in range(grid["rows"]):
-        for col in range(grid["columns"]):
-            rect = pygame.rect.Rect(grid["screen_pos"][0] + row * (grid["block_size"] + grid["line_width"]),
-                                    grid["screen_pos"][1] + col * (grid["block_size"] + grid["line_width"]),
-                                    grid["block_size"], grid["block_size"])
-            pygame.draw.rect(win, grid["bg_color"], rect)
+    for row in range(grid.rows):
+        for col in range(grid.columns):
+            rect = pygame.rect.Rect(grid.screen_pos.x + row * (grid.block_size + grid.line_width),
+                                    grid.screen_pos.y + col * (grid.block_size + grid.line_width),
+                                    grid.block_size, grid.block_size)
+            pygame.draw.rect(win, grid.bg_color, rect)
 
 
 if __name__ == "__main__":
--- gui.py
+++
@@ -1,6 +0,0 @@
-from PyQt5 import QtWidgets, uic
-
-qtMainWindow = "mainwindow.ui"
-Ui_MainWindow, QtBaseClass = uic.loadUiType(qtMainWindow)
-
-class Gui(QtWidgets.QMainWindow, Ui_MainWindow):
